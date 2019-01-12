5fc5317c436fa291f69fa82fc8fb6aad933a5c4f
BionicRobotsFLL2018GitHub <44321733+FLLGitHub@users.noreply.github.com>
2019-01-11 19:42:33 -0500

    Update Version 2019.1.1.py

--- Version 2019.1.1.py
+++ Version 2019.1.1.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageChops, ImageFilter
 import cv2
 import numpy as np
+import tkinter
 from tkinter import *
 from PIL import ImageTk, Image
 import PIL.Image
@@ -8,71 +9,58 @@ import PIL.ImageTk
 from tkinter import Tk
 from tkinter import filedialog
 from tkinter import ttk
+from tkinter.filedialog import askopenfilename
+
 
 # ____________________________________________________________________
 
+imagelink1 = None
+imagelink1 = lambda: askopenfilename()
+
+def openlink1():
+    imagelink1 = askopenfilename()
+
+def openlink2():
+    imagelink2 = askopenfilename()
 
 
-def diff_images():
+def diff1():
 
     #Opening Images
-    img1 = Image.open('Picture1.1.jpg')
-    img2 = Image.open('Picture1.2.jpg')
+    img1 = Image.open(imagelink1)
+    img2 = Image.open(imagelink2)
 
     #Subtracting Images
     diff1 = ImageChops.subtract(img1, img2)
 
     #Saving the subtraction
     diff1.save('diff1.png')
-
-    #Finding image parameters
-    width, height = img1.size
-
-    #Finding the number of black pixels in the subtraction
-    diff1 = cv2.imread('diff1.png', cv2.IMREAD_GRAYSCALE)
-    n_black_pix = np.sum(diff1 == 0)
-    print(width * height)
-    print(n_black_pix)
-    other_pixels = int(width * height - n_black_pix)
-
-    #Subtracting that from the total amount of pixels
-    print(other_pixels)
-    hole_size = int(other_pixels / 10000)
-
-    #Ranges of hole
-    if hole_size >= 100:
-      print("RED ALERT - Life threatning")
-    if 100 > hole_size >= 50:
-      print("YELLOW ALERT - Major Cut")
-    if hole_size < 50:
-      print("GREEN ALERT - Minor cut")
-
-    #Finding the edges by setting edge parameters
-    img = cv2.imread('diff1.png', 0)
-    edges = cv2.Canny(img, 100, 200)
-    cv2.imwrite('edges of image.png', edges)
-
-
-def diff1():
+    
     #Opening subtraction image
     diff1 = Image.open("diff1.png")
     diff1.show()
 
 def edges():
-    #Opening subtraction image
-    edges = Image.open('edges of image.png')
-    edges.show()
+    img1 = Image.open(imagelink1)
+    img2 = Image.open(imagelink2)
 
-def image_selection():
-    #selcting the image wanted
-    window.directory = filedialog.askdirectory()
-    print(window.directory)
+    #Subtracting Images
+    diff1 = ImageChops.subtract(img1, img2)
+
+    #Saving the subtraction
+    diff1.save('diff1.png')
+    
+    #Finding the edges by setting edge parameters
+    img = cv2.imread('diff1.png', 0)
+    edges = cv2.Canny(img, 100, 200)
+    cv2.imwrite('edges_of_image.png', edges)
+
 
     
 # _______________________________________________________________
 
 #Running function
-diff_images()
+
 
 #Setting geeral settings of tkinter window
 window = Tk()
@@ -81,7 +69,7 @@ window.attributes("-fullscreen", True)
 window.configure(bg = "#FBFBFB")
 
 #Creating Title
-Product_Title = Label(window, text = "Robin-15", font = "helvetica 40", bg = "#FBFBFB", )
+Product_Title = Label(window, text = "Robin 15", font = "helvetica 40", bg = "#FBFBFB", )
 Product_Title.pack()
 
 #Creating Space
@@ -111,7 +99,7 @@ space3.pack()
 #Creating Button
 Picture_Select_1_Button = Button(window, text = "Select Original Image", bg = "#F36F17", fg = "#FFFFFF",
            font = "helvetica 20", relief = 'flat', width = "15",
-           command = lambda: image_selection())
+           command = lambda: openlink1())
 Picture_Select_1_Button.pack()
 
 #Creating Space
@@ -119,9 +107,9 @@ space4 = Label(window, text = "", font = "helvetica 20", bg = "#FBFBFB", )
 space4.pack()
 
 #Creating Button
-Picture_Select_2_Button = Button(window, text = "Select Original Image", bg = "#F36F17", fg = "#FFFFFF",
+Picture_Select_2_Button = Button(window, text = "Select Changed Image", bg = "#F36F17", fg = "#FFFFFF",
            font = "helvetica 20", relief = 'flat', width = "15",
-           command = lambda: image_selection())
+           command = lambda: openlink2())
 Picture_Select_2_Button.pack()
 
 
